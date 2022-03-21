8b6187802dfdb145e0ffc442ccbe36b70cb5406c
John Doe <johndoe@example.com>
2022-03-21 15:55:08 +0000

    .

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo Starting Entrypoint Script
+ls -la /etc/apache2/sites-available
 cp -an /etc/apache2/sites-available-bak/. /etc/apache2/sites-available/
 rm -R /etc/apache2/sites-available-bak
 cp -an /var/www/html-bak/. /var/www/html/
 rm -R /var/www/html-bak
-# chmod 775 /var/www/html/ndxzsite/config/changeConfigVals.sh
-# /var/www/html/ndxzsite/config/changeConfigVals.sh
+
+
 # /usr/sbin/apache2ctl -D FOREGROUND
 # service apache2 start
-exec apache2-foreground
-exec "$@"
+
+# exec apache2-foreground
+# exec "$@"
 echo Entrypoint End
---
+++ build/startapp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo HELLO
+exec /usr/sbin/apache2
